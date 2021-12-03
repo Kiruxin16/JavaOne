77cc2ce82d82c06f0edaa9d4565368966a428e33
Kirill <kiruxin16@gmail.com>
2021-12-04 03:08:09 +0700

    В папках lesson 1..n выполнены задания Main для запуска

---
+++ src/Lesson5/Employee.java
@@ -0,0 +1,35 @@
+package Lesson5;
+
+public class Employee {
+    private String fio;
+    private String position;
+    private String eMail;
+    private String phoneNum;
+    int salary;
+    int age;
+
+    public Employee(String fio, String position,String eMail, String phoneNum, int salary, int age ){
+        this.fio=fio;
+        this.position=position;
+        this.eMail=eMail;
+        this.phoneNum=phoneNum;
+        this.salary=salary;
+        this.age=age;
+    }
+    public void info(){
+        System.out.println(this.toString());
+    }
+    @Override
+    public String toString(){
+        return             "Сотрудник:  "+ fio+"\n"+
+                "Должность:  "+ position+"\n"+
+                "Эл. почта:  "+ eMail+"\n"+
+                "Телефон:    "+ phoneNum+"\n"+
+                "Зарплата:   "+ salary+"\n"+
+                "Возраст:    "+ age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
