67d6299d6d558bca3eb410d7feece21984a3cebd
Kirill <kiruxin16@gmail.com>
2021-12-13 19:23:31 +0700

    В папках lesson 1..n выполнены задания Main для запуска

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
 //import Lesson3.ThirdHW;
 import Lesson5.Employee;
 import java.util.Arrays;
-import Lesson6.*;
+
+import Lesson7.*;
 public class Main {
     public static void main(String[] args) {
 
