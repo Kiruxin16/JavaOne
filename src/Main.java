c7c214ddaf33fc9dce3c48d94d4b62a1ac660126
Kirill <kiruxin16@gmail.com>
2021-12-13 19:26:19 +0700

    В папках lesson 1..n выполнены задания Main для запуска

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,27 @@ import java.util.Arrays;
 import Lesson7.*;
 public class Main {
     public static void main(String[] args) {
+        Bowl bowl = new Bowl(120);
+        Cat[] strongAndIndep = new Cat[]{
+                new Cat("Жора",35),
+                new Cat("Барсик", 15),
+                new Cat("Семен",20),
+                new Cat("Фаня",30),
+                new Cat("Маркиз", 15),
+                new Cat("Лева",10),
+                new Cat("Леопольд",30),
+                new Cat("Тоша", 15),
+                new Cat("Пушок",10)
+        };
+        for (Cat cat:strongAndIndep) {
+            cat.eatFood(bowl);
+            bowl.foodInfo();
+        }
+        bowl.addFood(120);
+        for (Cat cat:strongAndIndep) {
+            cat.eatFood(bowl);
+            bowl.foodInfo();
+        }
 
 
     }
